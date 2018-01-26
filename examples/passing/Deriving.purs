4e2aec049bf0be38b9411c26869f6690adc3a232
Javier Casas <javcasas@gmail.com>
2018-01-26 10:48:52 -0500

    Failing test

--- examples/passing/Deriving.purs
+++ examples/passing/Deriving.purs
@@ -22,6 +22,14 @@ newtype Z = Z { left :: X, right :: X }
 
 derive instance eqZ :: Eq Z
 
+data YearMonth = YearMonth {
+    year :: Int,
+    month :: Int
+  }
+
+derive instance eqYearMonth :: Eq YearMonth
+derive instance ordYearMonth :: Ord YearMonth
+
 main = do
   assert $ X 0 == X 0
   assert $ X 0 /= X 1
@@ -31,6 +39,7 @@ main = do
   assert $ X 0 < Y "Foo"
   assert $ Y "Bar" < Y "Baz"
   assert $ z == z
+  assert $ YearMonth {year: 2018, month: 1} > YearMonth {year: 2017, month: 12}
   log "Done"
   where
   z = Z { left: X 0, right: Y "Foo" }
